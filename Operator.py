91c5ac95efbc53ed58be5078b250b13334bf14c6
noemotion85 <kmg3833@naver.com>
2021-11-25 10:23:10 +0900

    gui_mk001_21/11/25 Main window created & title is "S.E.M.S"(Solar Electricity Monitoring System) & default size (1000 x 750 (4 : 3))

--- Operator.py
+++ Operator.py
@@ -1,2 +1,16 @@
-from tkinter.ttk import *
-#aaa
+#### Operator가 메인입니다.
+import tkinter
+from tkinter import ttk
+
+main_window = tkinter.Tk()
+
+screen_width = main_window.winfo_screenwidth()
+screen_height = main_window.winfo_screenheight()
+
+default_width = 1000
+default_height = 750
+
+main_window.title("S.E.M.S")
+main_window.geometry(f"{default_width}x{default_height}")
+
+main_window.mainloop()
