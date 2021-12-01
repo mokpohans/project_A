8c7725278905c9ef5ef7b8e517b86e79f422f807
noemotion85 <kmg3833@naver.com>
2021-12-01 17:50:01 +0900

    이미지선택기에 문제있음 분석 필요, 베이스 레이블 외에 구역 설계 다시해보기(배치 엉망됨)

--- Sys/Activity/MainPage.py
+++ Sys/Activity/MainPage.py
@@ -7,6 +7,11 @@ from Sys.Components import AdditionalWidgets as adwz
 class Mainpage:
     def __init__(self, page):
         self._mainpage = page
+        self._mainpage.grid_propagate(False)
+        self._mainpage.pack_propagate(False)
+        self._mainpage.columnconfigure(index=0, weight=1)
+        for i in range(0, 3, 1):
+            self._mainpage.rowconfigure(index=i, weight=1)
         self._mainpage.update()
         self._display_width = self._mainpage.winfo_screenwidth()
         self._display_height = self._mainpage.winfo_screenheight()
@@ -22,26 +27,26 @@ class Mainpage:
     def operate(self):
         #### 베이스 틀
             ### 메뉴, 시간, 날씨 포함하는 베이스 레이블
-        self.menu_base_label = ttk.Label(self._mainpage, width=self._window_width)
+        self.menu_base_Frame = ttk.Frame(self._mainpage)
             ### 시간, 발전소 선택 베이스 레이블
-        self.middle_base_label = ttk.Label(self._mainpage) # 좌->우 순서
+        self.middle_base_Frame = ttk.Frame(self._mainpage) # 좌->우 순서
             ### 발전량, 발전금액 등 상태 레이블을 포함하는 베이스 레이블
-        self.content_base_label = ttk.Label(self._mainpage)
+        self.content_base_Frame = ttk.Frame(self._mainpage)
 
-        self.menu_base_label.pack(expand=True, anchor=tk.E)
-        self.middle_base_label.pack(expand=True, anchor=tk.W)
+        self.menu_base_Frame.grid(row=0, column=0, sticky=tk.NSEW)
+        self.middle_base_Frame.grid(row=1, column=0, sticky=tk.NSEW)
         ## 날씨 표현 레이블
         # self.weather_describe_label = ttk.Label(self._mainpage)
         # self.weather_describe_label.pack(expand=True)
-        self.content_base_label.pack(expand=True)
+        self.content_base_Frame.grid(row=2, column=0, sticky=tk.NSEW)
 
         ### 메뉴 베이스 채우기
             # 메뉴 선택 레이블
-        self.menu_label = ttk.Label(self.menu_base_label, width=self._window_width)
+        self.menu_label = ttk.Label(self.menu_base_Frame, width=self._window_width)
         self.menus = adwz.linearmenu(self.menu_label, 3, texts=['계측정보', '보고서', '장애목록'])
 
             # 시간 관련 베이스 레이블
-        self.time_base_label = ttk.Label(self.middle_base_label)
+        self.time_base_label = ttk.Label(self.middle_base_Frame)
 #테스팅
         self.timepart = adwz.temp_Dclock(self.time_base_label, width=1000, height=100)
 
@@ -51,7 +56,7 @@ class Mainpage:
 #테스팅
 
             # 발전소 선택 레이블
-        self.plant_select_label = ttk.Label(self.middle_base_label)
+        self.plant_select_label = ttk.Label(self.middle_base_Frame)
 #테스팅
         self.temp_plant = adwz.temp_imagechooser(self.plant_select_label, image='./Resources/images/temp_bird.png', indications=['a', 'b', 'c'], anchor=tk.NE)
 #테스팅
@@ -70,9 +75,9 @@ class Mainpage:
 
         ### 컨텐츠 베이스 채우기
             ## 발전 상태 레이블
-        self.generate_state_label = ttk.Label(self.content_base_label)
+        self.generate_state_label = ttk.Label(self.content_base_Frame)
             ## 인버터 상태 레이블
-        self.invertor_state_label = ttk.Label(self.content_base_label)
+        self.invertor_state_label = ttk.Label(self.content_base_Frame)
 
         self.generate_state_label.pack(expand=True)
         self.invertor_state_label.pack(expand=True)
